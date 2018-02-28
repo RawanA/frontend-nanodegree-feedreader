c5ee58ad53194b186e165e436b364089e80f706f
RawanA <rawan.abdullah.bs@hotmail.com>
2018-03-01 02:26:57 +0300

    Update feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -86,7 +86,6 @@ $(function() {
             expect($("body").hasClass("menu-hidden")).toBeFalsy();
             Icon.click();
             expect($("body").hasClass("menu-hidden")).toBeTruthy();
-
     });
     /* TODO: Write a new test suite named "Initial Entries" */
 
